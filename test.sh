417eae47c79666ff9a4a76ef9bc966da60f66881
Okazaki Kohei <d20037@e.hiroshima-cu.ac.jp>
2021-07-01 22:07:59 +0900

    update

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 
 #RATE Mbps
 #DELAY ms
-
+# shellcheck disable=SC2188
+<< COMMENTOUT
 for r in 10 50 100
 do
   rate=$(("${r}"*1024*1024))
@@ -16,8 +17,8 @@ do
     sudo ip netns exec server tc qdisc add dev server_net0 parent 1:1 handle 10:1 netem delay ${delay}ms
     sudo ./ns_client/client.sh s_r${r}_d${d}
   done
-  echo
 done
+COMMENTOUT
 
 for r in 10 50 100
 do
@@ -32,24 +33,22 @@ do
     sudo ip netns exec client tc qdisc add dev client_net0 parent 1:1 handle 10:1 netem delay ${delay}ms
     sudo ./ns_client/client.sh c_r${r}_d${d}
   done
-  echo
 done
 
-for r in 10 50 100
+for R in 10 50 100
 do
-  rate=$(("${r}"*1024*1024))
+  rate=$(("${R}"*1024*1024))
   burst=$((("${rate}"/8)/250))
   limit=$(("${burst}"*10))
-  for d in 10 50 100
+  for D in 10 50 100
   do
-    delay=${d}
+    delay=${D}
     sudo ip netns exec server tc qdisc del dev server_net0 root
     sudo ip netns exec server tc qdisc add dev server_net0 root handle 1:0 tbf rate ${rate}bit burst ${burst}b limit ${limit}b
     sudo ip netns exec server tc qdisc add dev server_net0 parent 1:1 handle 10:1 netem delay ${delay}ms
     sudo ip netns exec client tc qdisc del dev client_net0 root
     sudo ip netns exec client tc qdisc add dev client_net0 root handle 1:0 tbf rate ${rate}bit burst ${burst}b limit ${limit}b
     sudo ip netns exec client tc qdisc add dev client_net0 parent 1:1 handle 10:1 netem delay ${delay}ms
-    sudo ./ns_client/client.sh sc_r${r}_d${d}
+    sudo ./ns_client/client.sh sc_r${R}_d${D}
   done
-  echo
 done
