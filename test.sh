8ead3007f1682b395407cbf2ebc68e533451bacf
Okazaki Kohei <d20037@e.hiroshima-cu.ac.jp>
2021-07-02 11:15:24 +0900

    update

--- test.sh
+++ test.sh
@@ -13,13 +13,14 @@ do
   do
     delay=${d}
     sudo ip netns exec server tc qdisc del dev server_net0 root
+    sudo ip netns exec client tc qdisc del dev client_net0 root
     sudo ip netns exec server tc qdisc add dev server_net0 root handle 1:0 tbf rate ${rate}bit burst ${burst}b limit ${limit}b
     sudo ip netns exec server tc qdisc add dev server_net0 parent 1:1 handle 10:1 netem delay ${delay}ms
     sudo ./ns_client/client.sh s_r${r}_d${d}
   done
 done
 COMMENTOUT
-
+<< COMMENTOUT
 for r in 10 50 100
 do
   rate=$(("${r}"*1024*1024))
@@ -28,13 +29,14 @@ do
   for d in 10 50 100
   do
     delay=${d}
+    sudo ip netns exec server tc qdisc del dev server_net0 root
     sudo ip netns exec client tc qdisc del dev client_net0 root
     sudo ip netns exec client tc qdisc add dev client_net0 root handle 1:0 tbf rate ${rate}bit burst ${burst}b limit ${limit}b
     sudo ip netns exec client tc qdisc add dev client_net0 parent 1:1 handle 10:1 netem delay ${delay}ms
     sudo ./ns_client/client.sh c_r${r}_d${d}
   done
 done
-
+COMMENTOUT
 for R in 10 50 100
 do
   rate=$(("${R}"*1024*1024))
